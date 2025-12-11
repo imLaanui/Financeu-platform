0dcb6351f051ea5750593b3f7dfd350c45e2905b
Justin Smith <jws8@hawaii.edu>
2025-12-10 23:44:13 -1000

    careers page redone

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Careers from "./pages/Careers";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/careers" element={<Careers />} />
     </Routes>
   </BrowserRouter>
 );
