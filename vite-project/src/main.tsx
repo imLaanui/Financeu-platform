32397b390fdc66c74c25633107b2c5c0b99c4681
Justin Smith <jws8@hawaii.edu>
2025-12-10 23:37:09 -1000

    contact page remade

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -2,12 +2,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>
 );
