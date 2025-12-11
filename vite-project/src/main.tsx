4ea11bc141ba8ab5ad3afe9313afa03971170376
Justin Smith <jws8@hawaii.edu>
2025-12-10 23:25:03 -1000

    about us page remade

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   </BrowserRouter>
 );
